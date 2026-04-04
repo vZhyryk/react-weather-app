94bf1211888e33282f99db9127b2c25b5ba57fc6
Vitaliy Zhyryk <vitaliy.zhyryk@gmail.com>
2026-04-04 21:40:51 +0200

    Update Idle.jsx

--- src/components/Idle/Idle.jsx
+++ src/components/Idle/Idle.jsx
@@ -2,7 +2,7 @@ import { IdleMessage } from './Idle.styled';
 export default function Idle() {
   return (
     <IdleMessage>
-      <h2>Search weather by location</h2>
+      <h2>Search weather by location now</h2>
     </IdleMessage>
   );
 }
